48c0a5dfab29329fa23ffc254246e83768dfcf58
{AbdulMubinDev} <{abdulmubin.dev@gmail.com}>
2025-07-06 02:48:44 +0600

    Some bug fixing and changing to class

--- calculator_gui.py
+++ calculator_gui.py
@@ -3,82 +3,94 @@ from sympy import sympify
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QVBoxLayout, QPushButton, QGridLayout, QHBoxLayout, QLabel, QLineEdit, QWidget
 
-#creating major parts
-app = QApplication([])
-main_window = QWidget()
-main_window.setWindowTitle("Calculator App")
-main_window.resize(300, 400)
 
+class CalculatorApp(QWidget):
+    def __init__(self):
+        super().__init__()
 
-def button_clicked():
-    button = app.sender()
-    text =  button.text()
+        self.setWindowTitle("Calculator App")
+        self.resize(300, 400)
 
-    if text == "=":
-        symbol = text_box.text()
-        try: 
-            exp = sympify(symbol)
-            value = exp.evalf()
-            res = f"{value:.2f}".rstrip('0').rstrip('.')
-            text_box.setText(str(res))
-        except Exception as e:
-            text_box.setText("Error")
-    elif text == "clear":
-        text_box.clear()
-    elif text == "del":
-        curr_input = text_box.text()
-        if curr_input == "Error":
-            text_box.clear()
-        else:
-            text_box.setText(curr_input[:-1])
-    elif text.isdigit:
-        curr_input = text_box.text()
-        if curr_input == '0':
-            text_box.setText(text)
-        else:
-            text_box.setText(curr_input + text)
-    else:
-        curr_input = text_box.text()
-        text_box.setText(curr_input + text)
+        self.text_box = QLineEdit()
+        self.create_buttons()
+        self.setup_layout()
 
+    def create_buttons(self):
+        self.grid = QGridLayout()
+        self.buttons = ["7", "8", "9", "/", 
+                    "4", "5", "6", "*", 
+                    "1", "2", "3", "-", 
+                    "0", ".", "=", "+"]
+        self.clear_btn = QPushButton("clear")
+        self.clear_btn.clicked.connect(self.button_clicked)
+        self.delete_btn = QPushButton("del")
+        self.delete_btn.clicked.connect(self.button_clicked)
 
-#ready to assemble
-text_box = QLineEdit()
-grid = QGridLayout()
-buttons = ["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+"]
-clear = QPushButton("clear")
-clear.clicked.connect(button_clicked)
-delete = QPushButton("del")
-delete.clicked.connect(button_clicked)
-
-col = 0 
-row = 0
-
-for text in buttons:
-    button = QPushButton(text)
-    button.clicked.connect(button_clicked)
-    grid.addWidget(button, row, col)
-    col += 1
-
-    if col > 3:
         col = 0 
-        row +=1
+        row = 0
+
+        for text in self.buttons:
+            self.button = QPushButton(text)
+            self.button.clicked.connect(self.button_clicked)
+            self.grid.addWidget(self.button, row, col)
+            col += 1
+
+            if col > 3:
+                col = 0 
+                row +=1
 
 
-#Assembling
-master_layout = QVBoxLayout()
-master_layout.addWidget(text_box)
-master_layout.addLayout(grid)
 
-button_row = QHBoxLayout()
-button_row.addWidget(clear)
-button_row.addWidget(delete)
+    def button_clicked(self):
+        button = self.sender()
+        text =  button.text()
+
+        if text == "=":
+            symbol = self.text_box.text()
+            try: 
+                exp = sympify(symbol)
+                value = exp.evalf()
+                res = f"{value:.2f}".rstrip('0').rstrip('.')
+                self.text_box.setText(str(res))
+            except Exception as e:
+                self.text_box.setText("Error")
+        elif text == "clear":
+            self.text_box.clear()
+        elif text == "del":
+            curr_input = self.text_box.text()
+            if curr_input == "Error":
+                self.text_box.clear()
+            else:
+                self.text_box.setText(curr_input[:-1])
+        elif text.isdigit:
+            curr_input = self.text_box.text()
+            if curr_input == '0':
+                self.text_box.setText(text)
+            else:
+                self.text_box.setText(curr_input + text)
+        else:
+            curr_input = self.text_box.text()
+            self.text_box.setText(curr_input + text)
 
 
-#Decorate
-master_layout.addLayout(button_row)
-main_window.setLayout(master_layout)
+    def setup_layout(self):
+    
+        #Assembling
+        master_layout = QVBoxLayout()
+        master_layout.addWidget(self.text_box)
+        master_layout.addLayout(self.grid)
 
-#Burger
-main_window.show()
-app.exec_()
+        button_row = QHBoxLayout()
+        button_row.addWidget(self.clear_btn)
+        button_row.addWidget(self.delete_btn)
+
+
+        #Decorate
+        master_layout.addLayout(button_row)
+        self.setLayout(master_layout)
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = CalculatorApp()
+    window.show()
+    app.exec_()
